a848e5f8768e11d6c1febdec6ee62a64a1eb73cf
Sam Schnaible II <sam@Sams-MacBook-Pro.local>
2021-08-13 23:46:53 -0700

    feature: get 5 reviews

--- server/index.js
+++ server/index.js
@@ -4,12 +4,22 @@ const path = require('path');
 const port = 3000;
 const axios = require('axios');
 const auth = require('../config.js');
-var compression = require('compression')
+const compression = require('compression');
+const db = require('../db/db.js');
 
 app.use(express.json());
 app.use(compression());
-app.use(express.static(path.join(__dirname, '..', '...')));
+// app.use(express.static(path.join(__dirname, '..', '')));
 
+app.get('/reviews', (req, res) => {
+  db.getReviews((err, results) => {
+    if (err) {
+      console.log('SERVER ERROR: ', err);
+    } else {
+      res.status(200).send(results);
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
