2f3dc10f5226ba3dd101224fbcc35191a50d8e4b
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-06 12:40:05 -0700

    controller index

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const router = require('./routes.js');
 const port = 3000;
 const auth = require('../config.js');
 const compression = require('compression');
-const db = require('../db/db.js');
+// const db = require('../db/db.js');
 app.use(express.json());
 app.use(compression());
 app.use((req, res, next) => {
@@ -28,27 +28,27 @@ app.listen(port, () => {
 });
 
 
-app.get('/reviews', (req, res) => {
-  req.header.id = 17244;
-  db.getReviews(req.header.id, (err, results) => {
-    if (err) {
-      console.log('SERVER ERROR: ', err);
-    } else {
-      res.status(200).send(results);
-    }
-  })
-});
+// app.get('/reviews', (req, res) => {
+//   req.header.id = 17244;
+//   db.getReviews(req.header.id, (err, results) => {
+//     if (err) {
+//       console.log('SERVER ERROR: ', err);
+//     } else {
+//       res.status(200).send(results);
+//     }
+//   })
+// });
 
-app.get('/reviews/meta', (req, res) => {
-  req.header.id = 17132;
-  db.getReviewsMeta(req.header.id, (err, result) => {
-    if (err) {
-      console.log('SERVER ERROR; ', err);
-    } else {
-      res.status(200).send(result)
-    }
-  })
-});
+// app.get('/reviews/meta', (req, res) => {
+//   req.header.id = 17132;
+//   db.getReviewsMeta(req.header.id, (err, result) => {
+//     if (err) {
+//       console.log('SERVER ERROR; ', err);
+//     } else {
+//       res.status(200).send(result)
+//     }
+//   })
+// });
 
 app.put('/reviews/:review_id/helpful', (req, res) => {
   req.params.review_id = 1;
