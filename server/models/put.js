e2baec01af5b810608d052c465aba2f02b0adc9b
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-06 12:45:59 -0700

    refactor put queries, models

---
+++ server/models/put.js
@@ -0,0 +1,28 @@
+const { pool } = require('../../db');
+
+module.exports = {
+
+  updateReviewHelpful: function(id, callback) {
+    return pool.query(`UPDATE rate_review.reviews SET helpfulness =
+    helpfulness + 1 WHERE id = ${id}`, err => {
+      if (err) {
+        console.log('DATABASE ERROR; ', err);
+        callback(err);
+      } else {
+        callback(null);
+      }
+    });
+  },
+
+  updateReviewReported: function(id, callback) {
+    return pool.query(`UPDATE rate_review.reviews SET reported = true
+    WHERE id = ${id}`, err => {
+      if (err) {
+        console.log('DATABASE ERROR: ', err);
+        callback(err);
+      } else {
+        callback(null);
+      }
+    })
+  }
+}
--- server/routes.js
+++ server/routes.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 
 router.get('/reviews', controller.get.reviews);
 router.get('/reviews/meta', controller.get.reviewsMeta);
+
+router.put('/reviews/:review_id/helpful', controller.put.)
